28752e508a1307e02864a55e91dfd2f3ef12961e
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2021-09-12 16:49:16 +0300

    17 react file completed

---
+++ Front End Development Libraries/React/create-a-complex-jsx-element.js
@@ -0,0 +1,10 @@
+const JSX = 
+<div>
+  <h1>Enes!</h1>
+  <p>Ergun!</p>
+  <ul>
+    <li>A</li>
+    <li>B</li>
+    <li>C</li>
+  </ul>
+</div>
---
+++ Front End Development Libraries/React/create-a-stateless-functional-component.js
@@ -0,0 +1,12 @@
+const MyComponent = function() {
+  // Change code below this line
+  return (
+    <div>
+      <p>enes</p>
+    </div>
+  );
+
+
+
+  // Change code above this line
+}
